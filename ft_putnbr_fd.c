a42bab47b9b712956cd1c5f4ba1e215c3802ebc1
David de Fitero Teijeira <david@defitero.com>
2025-10-12 00:08:21 +0200

    fix for putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 23:25:08 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/10/11 23:28:31 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/10/12 00:08:07 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,24 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (n == INT_MIN)
+	{
+		ft_putnbr_fd(INT_MIN / 10, fd);
+		ft_putnbr_fd(-(INT_MIN % 10), fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd((n % 10) + '0', fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
 }
 
 // int	main(void)
@@ -28,5 +45,9 @@ void	ft_putnbr_fd(int n, int fd)
 // 	ft_putnbr_fd(INT_MAX, 1);
 // 	ft_putchar_fd('\n', 1);
 // 	ft_putnbr_fd(INT_MIN, 1);
+// 	ft_putchar_fd('\n', 1);
+// 	ft_putnbr_fd(0, 1);
+// 	ft_putchar_fd('\n', 1);
+// 	ft_putnbr_fd(10000, 1);
 // 	ft_putchar_fd('\n', 1);
 // }
