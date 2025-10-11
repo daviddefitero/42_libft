d5a3806900de5ac78f7c79c0cb682e8fc59abd5b
David de Fitero Teijeira <david@defitero.com>
2025-10-11 23:23:33 +0200

    test for putend

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 21:42:35 by david             #+#    #+#             */
-/*   Updated: 2025/09/14 21:48:55 by david            ###   ########.fr       */
+/*   Updated: 2025/10/11 23:23:17 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,9 @@ void	ft_putendl_fd(char *s, int fd)
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
 }
+
+// int	main(void)
+// {
+// 	ft_putendl_fd("Hello World!", 1);
+// 	ft_putendl_fd("I like flowers", 1);
+// }
