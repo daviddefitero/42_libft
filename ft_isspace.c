cac952f068279d21b7e6ae662c1dd090d175a8a9
David de Fitero Teijeira <david@defitero.com>
2025-10-12 15:29:53 +0200

    ft_isspace

--- ft_isspace.c
+++ ft_isspace.c
@@ -1,40 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/10 16:44:19 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/10/12 15:22:22 by dde-fite         ###   ########.fr       */
+/*   Created: 2025/10/12 15:19:53 by dde-fite          #+#    #+#             */
+/*   Updated: 2025/10/12 15:29:31 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int	ft_atoi(const char *nptr)
+int	ft_isspace(int c)
 {
-	int		nbr;
-	char	sign;
-
-	nbr = 0;
-	sign = 1;
-	while (ft_isspace(*nptr))
-		nptr++;
-	if (*nptr == '-' || *nptr == '+')
-		if (*nptr++ == '-')
-			sign = -sign;
-	while (ft_isdigit(*nptr))
-	{
-		nbr = nbr * 10 + (*nptr - '0');
-		nptr++;
-	}
-	return (nbr * sign);
+	return (c == ' ' || (c >= 9 && c <= 13));
 }
 
 // #include <stdio.h>
 
 // int	main(void)
 // {
-// 	printf("%d", ft_atoi("+2324"));
+// 	const char	a = 'J';
+// 	const char	b = ' ';
+// 	const char	c = '4';
+// 	const char	d = '\t';
+
+// 	printf("Test 1: %c is space? %d\n", a, ft_isspace(a));
+// 	printf("Test 2: %c is space? %d\n", b, ft_isspace(b));
+// 	printf("Test 3: %c is space? %d\n", c, ft_isspace(c));
+// 	printf("Test 4: %c is space? %d\n", d, ft_isspace(d));
 // }
