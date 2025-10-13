562e64c12d0d6748ac0e056cfd1359560224fe7c
David de Fitero Teijeira <david@defitero.com>
2025-10-13 19:35:44 +0200

    cleaned strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,24 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dde-fite <dde-fite@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 20:28:26 by david             #+#    #+#             */
-/*   Updated: 2025/10/09 19:15:16 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/10/13 19:35:31 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// int	ft_strncmp(const char *s1, const char *s2, size_t n)
-// {
-// 	while (n-- > 0 && (unsigned char)*s1 == (unsigned char)*s2 && (*s1 || *s2))
-// 	{
-// 		s1++;
-// 		s2++;
-// 	}
-// 	return ((unsigned char)*s1 - (unsigned char)*s2);
-// }
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
