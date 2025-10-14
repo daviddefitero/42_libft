156709417802353510ff0e6dc101eb208a34966a
David de Fitero Teijeira <david@defitero.com>
2025-10-14 22:19:43 +0200

    fix for strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dde-fite <dde-fite@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 16:46:07 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/10/10 17:04:43 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/10/14 22:19:16 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	const char	*_little = little;
-	const char	*cncdnce;
+	const char	*_big;
+	const char	*_little;
+	size_t		_len;
 
 	if (!*little)
 		return ((char *)big);
 	while (len > 0 && *big)
 	{
-		_little = little;
-		cncdnce = big;
-		while (len > 0 && *big++ == *_little++)
+		if (*big == *little)
 		{
-			if (!*_little)
-				return ((char *)cncdnce);
-			len--;
+			_big = big;
+			_little = little;
+			_len = len;
+			while (_len-- > 0 && *_big++ == *_little++)
+				if (!*_little)
+					return ((char *)big);
 		}
+		big++;
 		len--;
 	}
 	return (NULL);
@@ -36,9 +39,13 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 // int	main(void)
 // {
-// 	char *big = "abcdef";
-	// 	char *little = "abcdefghijklmnop";
-	// 	size_t max = strlen(big);
-
-// 	printf("%s", strnstr(big, little, max));
+// 	printf("Test 1: %s\n", ft_strnstr("hello good morning", "good", 15));
+// 	printf("Test 2: %s\n", ft_strnstr("hello how are u doing", " ", 6));
+// 	printf("Test 3: %s\n", ft_strnstr("hello how are u doing", " ", 1));
+// 	printf("Test 4: %%START%%%s%%END%%\n",
+// 		ft_strnstr("\n--a-\b--b-\n-c-\0-d--\0hi!\n", "\0", 15));
+// 	printf("Test 5: %s\n", ft_strnstr("", "", 15));
+// 	printf("Test 6: %s\n", ft_strnstr("abcdef", "abcdefghijklmnop", 17));
+// 	printf("Test 7: %s\n", ft_strnstr("aaabcabcd", "aabc", -1));
+// 	printf("Test 8: %s\n", ft_strnstr("aaabcabcd", "abcd", 9));
 // }
